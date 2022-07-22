50f383f93de50ed0061d0ee052aafcd0f2d15fe2
Millan Bhandari <millan.ib@icloud.com>
2022-07-22 11:25:21 -0500

    created animations for "X" and "O"

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -18,16 +19,23 @@ struct ContentView: View {
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15,  content: {
                 ForEach(0..<9) { index in
                     ZStack {
-                    Color.blue
-                    Text(moves[index])
+                        Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
+                        Text(moves[index])
                             .font(.system(size:90))
                             .fontWeight(.heavy)
                     }
-                        .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
-                        .onTapGesture {
-                            moves[index] = "X"
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(Animation.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
                         }
+                    }
                 }
                 
             })
